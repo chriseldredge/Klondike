4523b21b3002261b6ffa343ad0bb6d1c2c067071
Chris Eldredge <chris.eldredge@gmail.com>
2014-01-18 14:35:04 -0500

    Don’t show Sign In link until session is resolved.

--- app/js/appkit/controllers/application.js
+++ app/js/appkit/controllers/application.js
@@ -3,8 +3,11 @@ import BaseControllerMixin from 'mixins/baseControllerMixin';
 export default Ember.Controller.extend(BaseControllerMixin, {
     needs: 'packages/search',
     searchBoxBinding: Ember.Binding.oneWay('controllers.packages/search.query'),
+
     isLoggedInBinding: Ember.Binding.oneWay('App.session.isLoggedIn'),
     usernameBinding: Ember.Binding.oneWay('App.session.username'),
+    isSessionInitializedBinding: Ember.Binding.oneWay('App.session.isInitialized'),
+
     apiUrlBinding: Ember.Binding.oneWay('App.restApi.apiUrl'),
 
     actions: {
