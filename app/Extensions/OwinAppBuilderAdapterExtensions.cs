caaabb6951478e2491643a81ed9ed07730e45470
Chris Eldredge <chris.eldredge@gmail.com>
2014-07-11 16:23:10 +0100

    Expose vNext ApplicationStopping token to OWIN.

--- app/Extensions/OwinAppBuilderAdapterExtensions.cs
+++ app/Extensions/OwinAppBuilderAdapterExtensions.cs
@@ -7,6 +7,8 @@ using Owin;
 
 namespace Klondike.Extensions
 {
+    using Microsoft.AspNet.Hosting;
+    using Microsoft.Owin.BuilderProperties;
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
     using BuildFunc = Action<Func<Func<IDictionary<string, object>, Task>,
@@ -17,6 +19,10 @@ namespace Klondike.Extensions
         public static void UseOwinAppBuilder(this IBuilder builder, Action<IAppBuilder> action)
         {
             var appBuilder = new AppBuilder();
+            var applicationLifetime = (IApplicationLifetime) builder.ApplicationServices.GetService(typeof(IApplicationLifetime));
+            var appProperties = new AppProperties(appBuilder.Properties);
+            appProperties.AppName = builder.Server.Name;
+            appProperties.OnAppDisposing = applicationLifetime.ApplicationStopping;
 
             action(appBuilder);
 
