64029a6e6428d87cf2f7e6ec52bd97c91d9876cc
Chris Eldredge <chris.eldredge@gmail.com>
2014-07-11 04:57:43 +0100

    Migrate appSettings from Web.config to Settings.json.

--- app/KlondikeSettings.cs
+++ app/KlondikeSettings.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
+using Microsoft.Framework.ConfigurationModel;
 using NuGet.Lucene.Web;
 
 namespace Klondike
@@ -7,10 +10,25 @@ namespace Klondike
     public class KlondikeSettings : NuGetWebApiSettings
     {
         private readonly string applicationBase;
+        private static readonly IConfiguration config;
+        private static readonly NameValueCollection roleMappings;
+
+        static KlondikeSettings()
+        {
+            config = new Configuration().AddJsonFile("Settings.json");
+            roleMappings = config.GetSubKeys("roleMappings").Aggregate(new NameValueCollection(), (c, kv) => { c[kv.Key] = kv.Value.Get(null); return c; });
+        }
 
         public KlondikeSettings()
+            : base("", new NameValueCollection(), roleMappings)
         {
             applicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase ?? Directory.GetCurrentDirectory();
+        }
+
+        protected override string GetAppSetting(string key, string defaultValue)
+        {
+            var value = config[key];
+            return String.IsNullOrWhiteSpace(value) ? defaultValue : value;
         }
 
         protected override string MapPathFromAppSetting(string key, string defaultValue)
