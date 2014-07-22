957f8a0fe22995eac50fc70d2fea4a89f4793c35
Chris Eldredge <chris.eldredge@gmail.com>
2014-07-22 20:36:18 +0100

    Add console app for self-hosting.

---
+++ src/Klondike.SelfHost/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Owin.Hosting;
+
+namespace Klondike.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+            if (!int.TryParse(args.FirstOrDefault() ?? "8080", out port))
+            {
+                port = 8080;
+            }
+
+            var options = new StartOptions
+            {
+                ServerFactory = "Nowin",
+                Port = port
+            };
+
+            using (WebApp.Start<NuGet.Lucene.Web.Startup>(options))
+            {
+                Console.WriteLine("Running a http server on port {0}. Press enter to quit.", port);
+                Console.ReadLine();
+            }
+        }
+    }
+}
