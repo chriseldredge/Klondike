e02505e5a46b1f6c0b66da056e2a7db23a4d14a4
Chris Eldredge <chris.eldredge@gmail.com>
2014-08-13 18:25:59 +0100

    Disable logging in development. It can be turned on as needed.

--- config/environment.js
+++ config/environment.js
@@ -21,12 +21,12 @@ module.exports = function(environment) {
   ENV.APP.version = require('../package.json').version;
 
   if (environment === 'development') {
-    ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_MODULE_RESOLVER = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    //ENV.APP.LOG_RESOLVER = true;
+    //ENV.APP.LOG_ACTIVE_GENERATION = true;
+    //ENV.APP.LOG_MODULE_RESOLVER = true;
+    //ENV.APP.LOG_TRANSITIONS = true;
+    //ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    //ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'production') {
