a57867dfaa0ce66c70b1ac8e480124ddd4f84139
Chris Eldredge <chris.eldredge@gmail.com>
2014-08-19 13:56:58 -0400

    Clean up authorized-route
    - Fix file name
    - Transition to 'index' when not logged in
    - Don't do anything after aborting transition
    - Don't assume session promise resolves to itself

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
-import AuthorizedRoute from 'Klondike/mixins/authorizedRoute';
+import AuthorizedRoute from 'Klondike/mixins/authorized-route';
 
 export default Ember.Route.extend(AuthorizedRoute, {
     authorizedApiName: 'users.getAuthenticationInfo',
     model: function () {
-        return this.get('session');
+        var self = this;
+        return this.get('session').then(function() {
+            return self.get('session').get('user');
+        }, null, 'Route:\'profile\': get user from session');
     }
 });
--- app/routes/users/edit.js
+++ app/routes/users/edit.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import ProgressIndicatorRoute from 'Klondike/mixins/progress-indicator-route';
-import AuthorizedRoute from 'Klondike/mixins/authorizedRoute';
+import AuthorizedRoute from 'Klondike/mixins/authorized-route';
 
 export default Ember.Route.extend(ProgressIndicatorRoute, AuthorizedRoute, {
     authorizedApiName: 'users.post'
