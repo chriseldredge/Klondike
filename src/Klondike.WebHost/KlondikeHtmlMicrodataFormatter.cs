681e3e30bc23e5bb3f034a4a6a54e8f05cc49b61
Chris Eldredge <chris.eldredge@gmail.com>
2014-08-29 21:53:09 +0100

    Fixing css and js on /api explorer.

--- src/Klondike.WebHost/KlondikeHtmlMicrodataFormatter.cs
+++ src/Klondike.WebHost/KlondikeHtmlMicrodataFormatter.cs
@@ -12,29 +12,30 @@ namespace Klondike
 {
     public class KlondikeHtmlMicrodataFormatter : NuGetHtmlMicrodataFormatter
     {
-        private readonly Lazy<string> _cssFilename = new Lazy<string>(FindSylesheet);
+        private readonly Lazy<IList<string>> _css = new Lazy<IList<string>>(FindSylesheet);
 
-        private static string FindSylesheet()
+        private static List<string> FindSylesheet()
         {
-            const string stylePath = "~/styles/";
+            const string stylePath = "~/assets/";
             var cssDir = HostingEnvironment.MapPath(stylePath);
-            var first = Directory.GetFiles(cssDir, "*.css").FirstOrDefault();
-            if (first == null) return null;
-            var file = Path.GetFileName(first);
-            return VirtualPathUtility.ToAbsolute(stylePath + file);
+            var vendorCss = Directory.GetFiles(cssDir, "vendor*.css").FirstOrDefault();
+            var appCss = Directory.GetFiles(cssDir, "klondike*.css").FirstOrDefault();
+            return new[] {vendorCss, appCss}
+                .Where(i => i != null)
+                .Select(i => VirtualPathUtility.ToAbsolute(stylePath + Path.GetFileName(i)))
+                .ToList();
         }
 
         public override IEnumerable<XObject> BuildHeadElements(object value, HttpRequestMessage request)
         {
             var headElements = base.BuildHeadElements(value, request);
 
-            if (_cssFilename.Value == null) return headElements;
-            
-            var cssLink = new XElement("link",
-                new XAttribute("rel", "stylesheet"),
-                new XAttribute("href", _cssFilename.Value));
+            var cssLinks = _css.Value.Select(
+                i => new XElement("link",
+                        new XAttribute("rel", "stylesheet"),
+                        new XAttribute("href", i)));
 
-            return headElements.Union(new [] {cssLink});
+            return headElements.Union(cssLinks);
         }
     }
 }
