a35d64a56e7e45d42eaf697f079965d13c5c7a23
Chris Eldredge <chris.eldredge@gmail.com>
2014-10-22 13:52:22 -0400

    Move index markup back to index.html so it can be customized during install.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,11 +2,15 @@ import Ember from 'ember';
 import BaseControllerMixin from 'Klondike/mixins/base-controller';
 
 export default Ember.Controller.extend(BaseControllerMixin, {
-    packageSourceCommand: function() {
-        return 'nuget sources add -name Klondike -source ' + this.get('packageSourceUri');
-    }.property('packageSourceUri'),
+  packageSourceName: function() {
+    return Ember.$('#package-source-command-placeholder').attr('data-package-source-name');
+  }.property(),
 
-    packageSourceCommandWithPrompt: function() {
-        return 'C:\\> ' + this.get('packageSourceCommand');
-    }.property('packageSourceCommand')
+  packageSourceCommand: function() {
+    return 'nuget sources add -name ' + this.get('packageSourceName') + ' -source ' + this.get('packageSourceUri');
+  }.property('packageSourceUri'),
+
+  packageSourceCommandWithPrompt: function() {
+    return 'C:\\> ' + this.get('packageSourceCommand');
+  }.property('packageSourceCommand')
 });
