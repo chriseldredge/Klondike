7754edb8947c8f1e8509d9bac058bb9754915eb6
Chris Eldredge <chris.eldredge@gmail.com>
2014-10-24 15:51:27 -0400

    Append content instead of replacing and clean up lint errors.

--- app/views/index.js
+++ app/views/index.js
@@ -1,17 +1,16 @@
 import Ember from 'ember';
-import config from '../config/environment';
 
 export default Ember.View.extend({
   injectMarkup: function() {
-    console.log(command);
     var content = Ember.$('#index-content').clone();
 
-    var source = $(this.$().children()[0]).clone();
+    var source = Ember.$(this.$().children()[0]).clone();
     content.find('#package-source-placeholder').replaceWith(source);
 
-    var command = $(this.$().children()[1]).clone();
+    var command = Ember.$(this.$().children()[1]).clone();
     content.find('#package-source-command-placeholder').replaceWith(command);
 
-    this.$().replaceWith(content);
+    this.$().empty();
+    content.appendTo(this.$());
   }.on('didInsertElement')
 });
