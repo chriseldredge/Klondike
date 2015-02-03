307f4ac5b59d93b183893491f661a2220f049282
Chris Eldredge <chris.eldredge@gmail.com>
2015-02-03 17:50:06 -0500

    Stop progress indicator when route errors.

--- app/mixins/progress-indicator-route.js
+++ app/mixins/progress-indicator-route.js
@@ -2,12 +2,20 @@ import Ember from 'ember';
 import ProgressIndicator from 'klondike/progress-indicator';
 
 export default Ember.Mixin.create({
-    actions: {
-        loading: function() {
-            ProgressIndicator.start();
-            this.router.one('didTransition', function() {
-                ProgressIndicator.done();
-            });
-        }
+  start: ProgressIndicator.start,
+  done: ProgressIndicator.done,
+
+  actions: {
+    loading: function() {
+      this.start();
+    },
+
+    error: function() {
+      this.done();
+    },
+
+    didTransition: function() {
+      this.done();
     }
+  }
 });
