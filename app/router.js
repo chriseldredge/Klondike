e33a572683b4822c03cbc3ef8fe5cb6cfb61389c
Chris Eldredge <chris.eldredge@gmail.com>
2015-04-14 20:33:44 +0100

    Upgrade to ember-cli 0.2.3 and ember 1.11.1.

--- app/router.js
+++ app/router.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
-import config from './config/environment';
-
-var Router = Ember.Router.extend({
-  location: config.locationType
-});
-
-Router.map(function() {
+export default Ember.Router.map(function() {
     this.route('index', { path: '/' });
     this.route('login');
     this.route('denied');
@@ -25,5 +19,3 @@ Router.map(function() {
     });
     this.route('not_found', { path: '/*not_found' });
 });
-
-export default Router;
