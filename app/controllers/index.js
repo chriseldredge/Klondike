3ad332a92ee3ad7cb15e17e4b1aca317b5bc9e93
Chris Eldredge <chris.eldredge@gmail.com>
2015-08-21 12:26:48 -0400

    Remove boilerplate content on index view.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,11 +2,7 @@ import Ember from 'ember';
 import BaseControllerMixin from 'klondike/mixins/base-controller';
 
 export default Ember.Controller.extend(BaseControllerMixin, {
-  packageSourceName: function() {
-    return Ember.$('#package-source-command-placeholder').attr('data-package-source-name');
-  }.property(),
-
   packageSourceCommand: function() {
-    return 'nuget sources add -name ' + this.get('packageSourceName') + ' -source ' + this.get('packageSourceUri');
+    return 'nuget sources add -name Klondike -source ' + this.get('packageSourceUri');
   }.property('packageSourceUri')
 });
