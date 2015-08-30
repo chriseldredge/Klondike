4d20100c7e1579e380f762bc8ec1abbf8a8e3a45
Chris Eldredge <chris.eldredge@gmail.com>
2015-08-30 15:17:17 +0100

    Don't build DNX from ember-cli.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,5 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var msbuild = require('broccoli-msbuild');
 var select = require('broccoli-select');
 
 module.exports = function(defaults) {
@@ -26,41 +25,8 @@ module.exports = function(defaults) {
     });
   }
 
-  function msbuildTree() {
-    var msbuildInputTree = select('src', {
-      acceptFiles: [ '**/*.csproj', '**/*.cs', '**/*.config' ],
-      outputDir: '/build'
-    });
-
-    var versionParts = app.project.pkg.version.split('-');
-    var versionPrefix = versionParts[0];
-    var versionSuffix = versionParts.length > 1 ? versionParts[1] : '';
-
-    var config = require('./config/environment')(app.env);
-
-    if (config.disableMSBuild) {
-      return null;
-    }
-
-    return msbuild(msbuildInputTree, {
-      project: require('path').join(__dirname, 'Ciao.proj'),
-      toolsVersion: '4.0',
-      configuration: config.configuration,
-      properties: {
-        VersionPrefix: versionPrefix,
-        VersionSuffix:  versionSuffix,
-        DistDir: '{destDir}'
-      }
-    });
-  }
-
   function buildTrees() {
-      var trees = [assetTree()];
-      var msbuild = msbuildTree();
-      if (msbuild !== null) {
-          trees.push(msbuild);
-      }
-      return trees;
+      return [assetTree()];
   }
 
   // Use `app.import` to add additional libraries to the generated
